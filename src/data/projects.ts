56320ca56697b4a0c41f6482a47d62a406757845
Jardiv <jrlepardo3@gmail.com>
2025-02-11 22:57:55 +0800

    added clothing website

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,26 +1,32 @@
 export const projects = [
-    {
-        name: "Pompadour Barbershop",
-        tags: ["react", "tailwind"],
-        hoverImage: "/projects/project-video/barber-website.mp4",
-        image: "/projects/barber-website.png",
-        link: ""
-    },
-    {
-        name: "Real estate",
-        tags: ["react", "tailwind"],
-        hoverImage: "https://i.imgur.com/Ckr6u7Y.mp4",
-        image: "/projects/realestate-website.png",
-        link: ""
-    },
-    {
-        name: "Kofi",
-        tags: ["react", "tailwind"],
-        hoverImage: "/projects/project-video/coffee-website.mp4",
-        image: "/projects/coffee-website.png",
-        link: ""
-    }
+	{
+		name: "Pompadour Barbershop",
+		tags: ["react", "tailwind"],
+		hoverImage: "/projects/project-video/barber-website.mp4",
+		image: "/projects/barber-website.png",
+		link: "",
+	},
+	{
+		name: "Real estate",
+		tags: ["react", "tailwind"],
+		hoverImage: "https://i.imgur.com/Ckr6u7Y.mp4",
+		image: "/projects/realestate-website.png",
+		link: "",
+	},
+	{
+		name: "Kofi",
+		tags: ["react", "tailwind"],
+		hoverImage: "/projects/project-video/coffee-website.mp4",
+		image: "/projects/coffee-website.png",
+		link: "",
+	},
+	{
+		name: "ChillDraw",
+		tags: ["react", "tailwind"],
+		hoverImage: "/projects/project-video/clothing-website.mp4",
+		image: "/projects/clothing-website.png",
+		link: "",
+	},
 ];
-
 
 export default projects;
